74477d20e51e2dfd165e582e4e1fa78b925ce887
Thiago Souza <thiagocvs2003@gmail.com>
2026-08-03 18:27:05 +0100

    add syncthing folders

--- bash/.bashrc
+++ bash/.bashrc
@@ -9,6 +9,7 @@
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.local/share/npm-global/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
+export EDITOR=nvim
 
 RANGER_LOAD_DEFAULT_R=false #evita que ranger sobreponha o .config local com o global
 
@@ -37,6 +38,11 @@ alias ips='ip a | rg inet '
 alias ports='sudo netstat -tulanp'
 alias vim='nvim'
 alias vim.='nvim .'
+
+#vms and ssh
+alias debianvm='cd ~/vms/debian && quickemu --vm debian-13.6.0-netinst.conf'
+#alias debianvm='cd ~/vms/debian && quickemu --vm debian-13.6.0-netinst.conf | sleep 20 && ssh -v thiago@localhost -p 22220'
+alias debianvm-kill='cd ~/vms/debian && quickemu --vm debian-13.6.0-netinst.conf --kill'
 
 # navegation with cd
 alias ..='cd ..'
--- install.sh
+++ install.sh
@@ -1,207 +1,44 @@
-#!/bin/bash
-CONFIG_PATH="$HOME/.config"
-
+#!/usr/bin/env bash
 RED='\033[0;31m'
 GREY='\033[90m'
 GREEN='\e[32m'
-BLUE='\e[34m' 
+BLUE='\e[34m'
 NC='\033[0m' #sem cor, finaliza as cores
 BOLD="\e[1m"
 NORMAL="\e[0m"
 
-set -e
-
 cd "$(dirname "$0")"
 
-#menu verificação do sistema operacional
-#verifica o sistema operativo e define seu respectivo package manager
-if [[ "$OSTYPE" =~ ^linux ]]; then
-  if [ -f /etc/os-release ]; then    
-    source /etc/os-release
-    # A variável $ID contém o nome curto da distro (ex: ubuntu, debian, fedora)
-    case "$ID" in
-      ubuntu|debian)
-        echo -e "Sistema ${BOLD}Debian${NORMAL} detectado ($ID)"
-        PKG_MANAGER="sudo apt install"
-        THIS_OS_TYPE="debian"
-        ;;
-      fedora|rhel|centos)
-        echo -e "Sistema ${BOLD}RedHat${NORMAL} detectado ($ID)"
-        PKG_MANAGER="sudo dnf install"  
-        THIS_OS_TYPE="fedora"
-        ;;
-      alpine)
-        echo -e "Sistema ${BOLD}Alpine Linux${NORMAL} detectado"
-        PKG_MANAGER="apk add"
-        THIS_OS_TYPE="alpine"
-        ;;
-      arch)
-        echo -e "Sistema ${BOLD}Arch Linux${NORMAL} detectado"
-        PKG_MANAGER="sudo pacman -S"
-        THIS_OS_TYPE="arch"
-        ;;
-      *)
-        echo -e "Distribuição Linux não identificada: $ID"
-        echo -e "${GREY}Vou compilar o nvim mesmo assim, me diz se funcionou :D${NC}"
-        THIS_OS_TYPE="other_linux"
-        ;;
-    esac
-  else
-    echo -e "Arquivo /etc/os-release não encontrado. Sistema muito antigo?"
-    echo -e "${BOLD}${RED}Abortado${NC}${NORMAL}"
-  fi
-  #id do MacOS
-elif [[ "$OSTYPE" =~ ^darwin ]] then
-  echo -e  "Está a usar ${BOLD}MacOS${NORMAL}"
-  PKG_MANAGER="brew install"
-elif  [[ "$OSTYPE" =~ ^msys ||  "$OSTYPE" =~ ^cygwin  ]] then
-  echo -e "${RED}Script não continuará, sistema Windows detectado.${RC}"
-fi
-
-#Menu instalacao do stow 
-#caso nao tenha Stow instalado, instala com pkgmanager
-if ! command -v stow -v >/dev/null 2>&1; then
-  echo -e "${BOLD}GNU Stow${NORMAL} necessário para continuar, deseja instalar? (caso decidir que não, o script irá fechar, sem nenhuma alteração no sistema)\n"
-  read  -r  -p "(Y/n): " STOW_INPUT 
-  case "$STOW_INPUT" in
-    Y|y|s|S) 
-      echo -e "${BOLD}Instalando stow${NORMAL}"
-      $PKG_MANAGER stow
-      break
-      ;;    
-    N|n) 
-      echo -e "Stow não será instalado, saindo do script..."
-      exit
-      break
-      ;;
-  esac
-else
-  echo -e "${BOLD}GNU Stow${NORMAL} encontrado!"
-fi 
-
-#Instalacao do nvim
-if ! command -v nvim &> /dev/null; then
-  echo -e "Neovim não foi encontrado, instalando..."
-  # compilar neovim do repositorio oficial, em desenvolvimento
-
-  if  [[ $THIS_OS_TYPE =~ ^debian  || $THIS_OS_TYPE =~ ^fedora  ]] then  
-    echo -e  "Sistema $THIS_OS_TYPE detectado. Compilando Nvim do repositório oficial..."
-    # 1. Instala as dependências usando a sua variável (funciona para Debian e Fedora)
-    $PKG_MANAGER ninja-build cmake gcc g++ make unzip gettext curl git
-
-    # 2. Baixa, compila e instala o Neovim estável em uma única linha de comandos encadeados
-    rm -rf /tmp/neovim && git clone -b stable --single-branch https://github.com/neovim/neovim.git /tmp/neovim && cd /tmp/neovim && make CMAKE_BUILD_TYPE=Release && sudo make install
-    PKG_MANAGER  else
-    $PKG_MANAGER nvim
-  fi   
-  $PKG_MANAGER nvim
-fi
-
-#Funções principais
-#nvim
-function install_nvim {
-  if [[ -d "$CONFIG_PATH/nvim" ]]; then
-    echo -e "Há uma configuração ativa. Vai ser criado ${BOLD}"nvim-backup"${NORMAL} com as configurações antigas."
-
-    if [[ -e "$CONFIG_PATH/nvim-backup" ]]; then
-      echo -e "Já existe um backup em $CONFIG_PATH/nvim-backup"
-      echo -e "${RED}Abortando...${NORMAL}"
-      exit 1
-    fi
-
-    mv "$CONFIG_PATH/nvim" "$CONFIG_PATH/nvim-backup" && echo -e "Backup criado!" 
-    stow -v --target="$HOME" nvim && echo -e "${GREEN}Configuração instalada com sucesso!${NC}"
-  else 
-    stow -v --target="$HOME" nvim && echo -e "${GREEN}Configuração instalada com sucesso!${NC}"
-  fi 
-}
-
-#bashrc & blerc
-function install_bashrc {
-  if [[ -e "$HOME/.bashrc-backup" ]]; then
-    echo -e "Já existe um backup em $HOME/.bashrc-backup"
-    echo -e "${RED}Abortando...${NORMAL}"
-    return 1
-  fi
-
-  if type -t ble.sh &>/dev/null; then
-    echo -e "ble.sh está instalado e ativo!"
-  else
-    if [[ $THIS_OS_TYPE == "arch" ]]; then
-      $PKG_MANAGER blesh-git
-    else 
-      $PKG_MANAGER blesh
-    fi
-  fi
-
-  if [[ -e "$HOME/.blerc-backup" ]]; then
-    echo -e "Já existe um backup em $HOME/.blerc-backup"
-    echo -e "${RED}Abortando...${NORMAL}"
-    return 1
-  fi
-
-  #verifica se existe .bashrc, caso exista cria um backup
-  if [[ -f "$HOME/.bashrc" ]]; then
-    echo -e "Há uma configuração ativa. Vai ser criado ${BOLD}.bashrc-backup${NORMAL} com as configurações antigas."
-
-    mv "$HOME/.bashrc" "$HOME/.bashrc-backup" && echo -e "Backup criado!" 
-  fi
-
-  #mesma coisa mas com blerc
-  if [[ -f "$HOME/.blerc" ]]; then
-    echo -e "Há uma configuração ativa. Vai ser criado ${BOLD}.blerc-backup${NORMAL} com as configurações antigas."
-    mv "$HOME/.blerc" "$HOME/.blerc-backup" && echo -e "Backup criado!" 
-  fi
-
-  if stow -v --target="$HOME" bash; then
-    echo -e "Configuração instalada com sucesso!"
-  else
-    echo -e "${RED}Erro: Falha ao executar o GNU Stow.${NORMAL}"
-    return 1
-  fi
-}
-
-
-
-#Menu CLI
+printf "Bem vindo a instalação ${BOLD}dotfiles${NORMAL} do Thiago!\n"
+printf "Escolha: "
 printf "\n"
-
-printf "Escolha as configurações a serem instaladas:\n\n"
-
-printf "${BLUE}[0]${NC} Instalar todos\n"
-printf "${BLUE}[1]${NC} Neovim\n"
-printf "${BLUE}[2]${NC} .bashrc\n"
-
-#printf "\n"
-#printf '%*s\n' "$(tput cols)" '' | tr ' ' '='
-#printf "\n"
-
+printf "0 - dotfiles\n"
+printf "1 - Syncthing\n"
+printf "2 - Ambos\n"
 printf "\n"
+read -r -p ">: " INPUT
 
-printf "${GREY}É necessário ter o nvim na última versão para as configurações funcionarem sem problemas. Leia a documentação no Github para mais detalhes.${NC}"
 while true; do
-  printf "\n"
-  read -r -p ">: " INPUT
-
   case "$INPUT" in
-    0) 
-      echo -e "${BOLD}Instalando tudo...${NC}"
-      install_nvim && install_bashrc && echo -e "Instalados com sucesso" 
-      break
-      ;;  
-    1)
-      echo -e "Instalando neovim..."
-      install_nvim && echo -e "${GREEN}"
-      break
-      ;;  
-    2)
-      echo -e "${BOLD}Instalando .bashrc...${NC}"
-      install_bashrc
+    0)
+      echo "Chamando instalador..."
+      source scripts/dotfiles-stow.sh
       break
       ;;
-    *) 
-      echo -e "${RED}Opção Inválida.${NC}"
+    1)
+      echo "Chamando instalador..."
+      source scripts/syncthing.sh
       break
-      ;;  
+      ;;
+    2)
+      echo "Chamando instaladores..."
+      source scripts/dotfiles-stow.sh && source scripts/syncthing.sh
+      break
+      ;;
+    *)
+      echo "Input inválido"
+      break
+      ;;
   esac
+
 done
